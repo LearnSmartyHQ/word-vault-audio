3766793260791072807e746502844ec6100eb0fc
John Adib <mr.john.adib@gmail.com>
2025-01-18 13:08:06 +0000

    Refactor .gitignore to streamline ignored files and directories, removing unnecessary entries and consolidating relevant ones for improved project management.

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'eol-last': ['error', 'always'],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+];
